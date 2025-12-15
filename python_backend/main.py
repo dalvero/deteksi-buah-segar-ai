cd5e508968916ed06768262dc766ac4aefd2319a
Daniel Alvero <dalvero589@gmail.com>
2025-12-16 04:34:11 +0700

    use HF API via env variable

--- python_backend/main.py
+++ python_backend/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 # Setup CORS agar bisa diakses Next.js
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    # allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
